1aa666a93dc6f7d9b1fc392503f6c0fc95961303
Liam <liam@iux.com>
2025-06-04 16:58:54 +0700

    update...
    Signed-off-by: Liam <liam@iux.com>

--- app/src/main/java/com/liam/codereview/MainActivity.kt
+++ app/src/main/java/com/liam/codereview/MainActivity.kt
@@ -49,6 +49,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             text = "Hello $name!",
             modifier = modifier
         )
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
     }
 }
 
